bc047fc78d692f518e31b049b5ae944f9ec26416
unknown <akilapanunayan@gmail.com>
2024-07-29 06:56:50 +0100

    demo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,27 +14,39 @@ const Home = () => {
 				/>
 			</div>
 			<div className="h-auto w-auto absolute top-0 left-0">
-				<Floral1 color="white" className="w-[23vw] h-[23vw] object-contain " />
+				<Floral1
+					color="white"
+					className="w-[40vw] lg:w-[23vw] h-[40vw] lg:h-[23vw] object-contain "
+				/>
 			</div>
 			<div className="h-auto w-auto absolute bottom-0 right-0">
-				<Floral1 color="white" className="w-[23vw] h-[23vw] object-contain " />
+				<Floral1
+					color="white"
+					className="w-[40vw] lg:w-[23vw] h-[40vw] lg:h-[23vw] object-contain "
+				/>
 				{/* <Floral2 className="w-full h-full object-contain p-0" /> */}
 			</div>
 			<div className="h-auto w-auto absolute bottom-0 left-0">
 				{/* <img
 					src={images.f3}
 					alt="wedding"
-					className="w-[23vw] h-auto object-cover "
+					className="w-[40vw] lg:w-[23vw] h-[40vw] lg:h-auto object-cover "
 				/> */}
-				<Floral1 color="white" className="w-[23vw] h-[23vw] object-contain " />
+				<Floral1
+					color="white"
+					className="w-[40vw] lg:w-[23vw] h-[40vw] lg:h-[23vw] object-contain "
+				/>
 			</div>
 			<div className="h-auto w-auto absolute top-0 right-0">
 				{/* <img
 					src={images.f4}
 					alt="wedding"
-					className="w-[23vw] h-auto object-cover "
+					className="w-[40vw] lg:w-[23vw] h-[40vw] lg:h-auto object-cover "
 				/> */}
-				<Floral1 color="white" className="w-[23vw] h-[23vw] object-contain " />
+				<Floral1
+					color="white"
+					className="w-[40vw] lg:w-[23vw] h-[40vw] lg:h-[23vw] object-contain "
+				/>
 			</div>
 		</div>
 	);
